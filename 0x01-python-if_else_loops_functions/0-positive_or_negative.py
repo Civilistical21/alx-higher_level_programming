128d1f5c6050d3722bc0d29bb9b8f684104b780b
Civilistical21 <jamescephason@gmail.com>
2022-06-28 15:59:34 -0700

    Task 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-enter "number":
-
-if the number is greater than 0: is positive
-print(f"is positive {number > 0}")
-if the number is 0: is zero
-print(f"is positive {number = 0}")
-if the number is less than 0: is negative
-print(f"is positive {number < 0}")
+if number > 0:
+    print(number, 'is positive')
+elif number == 0:
+    print(number, 'is zero')
+else:
+    print(number, 'is negative')
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+def remove_char_at(str, n):
+
+#endif
