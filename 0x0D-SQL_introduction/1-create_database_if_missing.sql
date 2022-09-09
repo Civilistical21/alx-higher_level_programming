bf927f5151ae3934ad0a2971d9d03e041b79db4e
Civilistical22 <jamescephason@gmail.com>
2022-09-08 17:06:03 -0700

    task 1 and 2 fix

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- Creates the database hbtn_0c_0 in my MySQL sever.
-CREATE DATABASE IF NOT EXISTS 'tbtn_0c_0';
+-- Creates the database hbtn_0c_0 in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
